056eeed2d2b2b8a96e9727799bddfef974b620af
George <george.st.petersburg@gmail.com>
2023-08-07 18:08:13 +0300

    eng_version_added

--- Game_bot/script.js
+++ Game_bot/script.js
@@ -1,42 +1,55 @@
-/* eslint-disable prefer-const */
-/* eslint-disable semi */
-/* eslint-disable no-unused-vars */
+'use strict';
 
-'strict';
-
-const isNumber = function (str) {
-  // if (str == null) return false;
+const isNumber = (str) => {
+  if (str == null || str.trim() === '') return false;
   let num = Number(str);
   return !isNaN(parseFloat(num)) && isFinite(num);
 }
 
-function gameAdvancedBotFunc () {
+function gameAdvancedBotFunc() {
   let attempts = 10;
-  let getBotsNumber = () => Math.ceil(Math.random() * 100);
-  let botsNumber = getBotsNumber();
+  const getBotsNumber = () => Math.ceil(Math.random() * 100);
+  const botsNumber = getBotsNumber();
+  const langRu = confirm('Добро пожаловать в мини-игру "Угадай число."\nЧтобы продолжить на русском языке нажмите "OK".\n' +
+    '\nWelcome to a mini - game "Guess-the-Number".\nIf you want to coontinue in English press "cancel".');
 
-  // console.log(`Bots number is ${botsNumber}`); // for debugging
+  function afterSavedBotsNumber() {
+    const strArr = langRu ? [
+      `Угадайте число от 1 до 100. Введите число. У Вас есть ${attempts} попыток.`,
+      'Игра окончена. (Игрок нажал "Отмена").',
+      'Введи число!',
+      'Поздравляю, Вы угадали!!! Хотели бы сыграть еще?',
+      'Попытки закончились, хотите сыграть еще?',
+      'Загаданное число меньше. Осталось попыток:',
+      'Загаданное число больше. Осталось попыток:'
+    ] : [
+      `Guess a number from 1 to 100. Enter a number. You have ${attempts} attempts.`,
+      'The game is over. (Player has pressed "Cancel").',
+      'Enter a number!',
+      'Congratulations, you have guessed it right!!! Do you want to play more?',
+      'Attempts are over, do you want to play once again?',
+      'The secret number is smaller. Attempts left:',
+      'The secret number is bigger.  Attempts left:'
+    ];
 
-  function afterSavedBotsNumber () {
-    let usersInput = prompt(`Угадайте число от 1 до 100. Введите число. У Вас есть ${attempts} попыток.`);
+    let usersInput = prompt(strArr[0]);
     let restart = false;
 
     if (usersInput == null) {
-      alert('Игра окончена. (Игрок нажал "Отмена").');
+      alert(strArr[1]);
     } else if (!isNumber(usersInput)) {
-      alert('Введи число!');
+      alert(strArr[2]);
       afterSavedBotsNumber();
-    // eslint-disable-next-line eqeqeq
     } else if (botsNumber == usersInput) {
-      restart = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+      restart = confirm(strArr[3]);
       // the line below reads as 'if it was the last attempt and user had not guessed'
     } else if (attempts === 1) {
-      restart = confirm('Попытки закончились, хотите сыграть еще?')
+      restart = confirm(strArr[4])
     } else if (botsNumber < usersInput) {
-      alert(`Загаданное число меньше. Осталось ${--attempts} попыток`);
+      alert(`${strArr[5]} ${--attempts}`);
       afterSavedBotsNumber();
     } else if (botsNumber > usersInput) {
-      alert(`Загаданное число больше. Осталось ${--attempts} попыток`);
+      alert(`${strArr[6]} ${--attempts}`);
       afterSavedBotsNumber();
     }
 
@@ -45,4 +58,4 @@ function gameAdvancedBotFunc () {
   afterSavedBotsNumber();
 }
 
-const gameBot = gameAdvancedBotFunc();
+gameAdvancedBotFunc();
